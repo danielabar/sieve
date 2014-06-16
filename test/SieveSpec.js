34d5911b88d19ab36f9da6769c7966b3cc53f61a
Daniela Baron <dbaron@oculusinfo.com>
2014-06-16 14:21:05 -0400

    woohoo got chai working

--- test/SieveSpec.js
+++ test/SieveSpec.js
@@ -1,12 +1,18 @@
-define(['../app/scripts/sieve'], function(fixture) {
+define(['../app/scripts/sieve', '../app/bower_components/chai/chai'], function(fixture, chai) {
 
-  // var expect = chai.expect;
+  var expect = chai.expect;
 
   describe('SieveSpec.js: ', function() {
 
     it('Module is defined', function() {
       expect(fixture).not.to.be.null;
       expect(fixture).not.to.be.undefined;
+    });
+
+    it('Calculates prime numbers', function() {
+      var upTo = 10;
+      var result = fixture.calculatePrimes(upTo);
+      expect(result).to.be.undefined;
     });
 
   });
