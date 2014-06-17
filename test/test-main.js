200d53d128d4a186e294aa9024fb34966c49825e
Daniela Baron <dbaron@oculusinfo.com>
2014-06-17 13:05:25 -0400

    cleanup unused bootstrap javascript

--- test/test-main.js
+++ test/test-main.js
@@ -23,58 +23,8 @@ require.config({
   // Log the values of moduleName, url before and after normalizing
   paths: {
     jquery: 'app/bower_components/jquery/jquery',
-    bootstrapAffix: '../bower_components/bootstrap/js/affix',
-    bootstrapAlert: '../bower_components/bootstrap/js/alert',
-    bootstrapButton: '../bower_components/bootstrap/js/button',
-    bootstrapCarousel: '../bower_components/bootstrap/js/carousel',
-    bootstrapCollapse: '../bower_components/bootstrap/js/collapse',
-    bootstrapDropdown: '../bower_components/bootstrap/js/dropdown',
-    bootstrapModal: '../bower_components/bootstrap/js/modal',
-    bootstrapPopover: '../bower_components/bootstrap/js/popover',
-    bootstrapScrollspy: '../bower_components/bootstrap/js/scrollspy',
-    bootstrapTab: '../bower_components/bootstrap/js/tab',
-    bootstrapTooltip: '../bower_components/bootstrap/js/tooltip',
-    bootstrapTransition: '../bower_components/bootstrap/js/transition',
     chai: '../bower_components/chai/chai',
     sinon: '../bower_components/sinon/lib/sinon'
-  },
-  shim: {
-    bootstrapAffix: {
-      deps: ['jquery']
-    },
-    bootstrapAlert: {
-      deps: ['jquery', 'bootstrapTransition']
-    },
-    bootstrapButton: {
-      deps: ['jquery']
-    },
-    bootstrapCarousel: {
-      deps: ['jquery', 'bootstrapTransition']
-    },
-    bootstrapCollapse: {
-      deps: ['jquery', 'bootstrapTransition']
-    },
-    bootstrapDropdown: {
-      deps: ['jquery']
-    },
-    bootstrapModal: {
-      deps: ['jquery', 'bootstrapTransition']
-    },
-    bootstrapPopover: {
-      deps: ['jquery', 'bootstrapTooltip']
-    },
-    bootstrapScrollspy: {
-      deps: ['jquery']
-    },
-    bootstrapTab: {
-      deps: ['jquery', 'bootstrapTransition']
-    },
-    bootstrapTooltip: {
-      deps: ['jquery', 'bootstrapTransition']
-    },
-    bootstrapTransition: {
-      deps: ['jquery']
-    }
   },
 
   // dynamically load all test files
